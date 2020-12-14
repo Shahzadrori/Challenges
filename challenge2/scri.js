49b91ee96f0734a36321e380940e02abf6059b21
Shahzad <shahzadrori@gmail.com>
2020-12-14 12:05:01 +0500

    simply declaring vars and then multiply it by 60 then calls a functon

---
+++ challenge2/scri.js
@@ -0,0 +1,10 @@
+// var i = document.querySelector('button');
+
+var mins,secs;
+var mins = prompt("Enter the value",mins);
+
+secs = mins * 60;
+dito();
+function dito(){
+    document.write(secs);
+};
