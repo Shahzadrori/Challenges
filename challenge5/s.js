5e9edb03d716b924371dea760cec2aeb42fe51ca
Shahzad <shahzadrori@gmail.com>
2020-12-14 22:08:21 +0500

    rearranging the files

---
+++ challenge5/s.js
@@ -0,0 +1,9 @@
+function area(){
+    var a = document.getElementById('box1').value;
+    var b = document.getElementById('box2').value;
+
+    var multi = (a * b)/2;
+    document.getElementById('result').value = multi;
+
+    
+};
---
+++ challenge7/sc.js
@@ -0,0 +1,5 @@
+var a = Number(prompt('Enter the value'));
+
+var b = a ** 3;
+
+alert(b);
