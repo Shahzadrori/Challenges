cb7ee1d368545090f6f934217e081d35ef6d81c3
Shahzad <shahzadrori@gmail.com>
2020-12-15 12:18:44 +0500

    Assining a function to claculate perimete of rectangle

---
+++ challenge12/sc.js
@@ -0,0 +1,7 @@
+var a = Number(prompt('Enter length'));
+var b = Number(prompt('Enter width'));
+function per(num1,num2){
+    var c = (num1 + num2)*2;
+    confirm('Your answer is ' + c);
+};
+per(a,b);
